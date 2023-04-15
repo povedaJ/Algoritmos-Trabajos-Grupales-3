6bd6997b7e14b6eed9e5c9d72af0ef595f0f896e
dpaes <dpaesanipena@gmail.com>
2023-04-15 16:21:44 -0600

    add test

--- lab4/src/test/java/domain/DoublyLinkedListTest.java
+++ lab4/src/test/java/domain/DoublyLinkedListTest.java
@@ -8,16 +8,16 @@ public class DoublyLinkedListTest {
     @Test
     public void test1() throws ListException {
         DoublyLinkedList list = new DoublyLinkedList();
-
-        list.add(new Course("IF-5100","Bases de datos",4));
-        list.add(new Course("IF-4101","Lenguajes app Comerciales",4));
-        list.add(new Course("IF-3100","Sistemas de informacion",3));
         list.add(new Course("IF-3001","Algoritmos",4));
         list.add(new Course("IF-4001","Sistemas Operativos",4));
         list.add(new Course("IF-2000","Programacion",4));
         list.add(new Course("IF-3000","Programacion 2",4));
         list.add(new Course("IF-4000","Arquitectura",3));
         list.add(new Course("IF-5000","Redes",3));
+        list.add(new Course("IF-5100","Bases de datos",4));
+        list.add(new Course("IF-4101","Lenguajes app Comerciales",4));
+        list.add(new Course("IF-3100","Sistemas de informacion",3));
+
 
         System.out.println("¿Existe Informática Aplicada, Id=IF-6201?"+list.contains(new Course("IF-6201","Informática Aplicada",4)));
         System.out.println("¿Sistemas de informacion, Id=IF-3100?"+list.contains(new Course("IF-3100","Sistemas de informacion",4)));
