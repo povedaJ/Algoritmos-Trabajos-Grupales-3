fcb55892cd33a198d4080b29d3a38bde5d58db95
povedaJ <66696920+povedaJ@users.noreply.github.com>
2023-04-22 02:35:55 -0600

    TestCircularLinkedList Listo

--- Laboratory5/src/main/java/domain/Employee.java
+++ Laboratory5/src/main/java/domain/Employee.java
@@ -4,6 +4,7 @@ import util.Utility;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -23,15 +24,65 @@ public class Employee {
     }
 
     public Integer getAge(Date date) {
+        //Los meses del Date son del 0 al 11
+        Date currentDate = new Date();
+        currentDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(); //Fecha Actual
+        if ((date.getMonth() + 1) < (currentDate.getMonth() + 1)) { //verifica si el mes de cumpleaños ya paso
+            return currentDate.getYear() - date.getYear();
+        } else {
+            if ((date.getMonth() + 1) == (currentDate.getMonth() + 1)) { // cumple el mes actual
+                if (date.getDate() <= currentDate.getDate()) { // verifica si ya paso el dia de cumpleaños
+                    return currentDate.getYear() - date.getYear();
+                } else {
+                    return currentDate.getYear() - date.getYear() - 1;
+                }} else {
+                return currentDate.getYear() - date.getYear() - 1;
+            }}}// hacer
 
+    public Integer getId() {
+        return id;
+    }
 
-        return null;
-    }// hacer
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
 
     @Override
     public String toString() {
         String result;
-        result = "(ID)" + id +
+        result ="\n"+"(ID)" + id +
                 "/(Name)" + lastName + "," + firstName +
                 "/(Birthday)" + util.Utility.dateFormat(birthday) +
                 "/(Title)" + title +
