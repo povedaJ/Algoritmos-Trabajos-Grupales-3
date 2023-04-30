70895c0448fa66ddae9c3331a332afc0ef491f75
povedaJ <66696920+povedaJ@users.noreply.github.com>
2023-04-30 14:37:06 -0600

    Lab6

---
+++ Lab6/src/main/java/ucr/lab/HelloApplication.java
@@ -0,0 +1,26 @@
+package ucr.lab;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        String css = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setTitle("Laboratory 6");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
