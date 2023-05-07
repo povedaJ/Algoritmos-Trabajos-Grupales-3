ec09ce79799465a940165341fd4418076facf70f
povedaJ <66696920+povedaJ@users.noreply.github.com>
2023-05-06 20:17:14 -0600

    test

--- Lab7/src/test/java/domain/LinkedQueueTest.java
+++ Lab7/src/test/java/domain/LinkedQueueTest.java
@@ -8,8 +8,6 @@ class LinkedQueueTest {
     void test1() {
         LinkedQueue queue= new LinkedQueue();
         SinglyLinkedList singly = new SinglyLinkedList();
-        singly.add("Ana");
-
         for (int i = 0; i < 30; i++) {
             singly.add(Utility.getPlace()); //30 nombres de lugares
         }
@@ -23,7 +21,6 @@ class LinkedQueueTest {
                 aStack.push(Utility.getObject());
             }
             queue.enQueue(singly);
-
             queue.enQueue(aQueue);
             queue.enQueue(aStack);
             System.out.println(queue.toString());
@@ -51,30 +48,5 @@ class LinkedQueueTest {
         }
     }
 
-    @Test
-    void test() {//
 
-        //enQueue: 10, 20,5,7,2,1
-        LinkedQueue queue= new LinkedQueue();
-//en el ultimo nivel(ya va a imprimir algo) se usa try cath
-        try {
-            queue.enQueue(10);
-            queue.enQueue(20);
-            queue.enQueue(5);
-            queue.enQueue(7);
-            queue.enQueue(2);
-            queue.enQueue(1);
-            System.out.println(queue);
-            System.out.println("Index of 5: "+ queue.indexOf(5));
-            System.out.println("Index of 2: "+ queue.indexOf(2));
-            System.out.println("Index of 1: "+ queue.indexOf(1));
-            System.out.println("Contains 15 : "+ queue.contains(15));
-            System.out.println( queue.contains(15)?"El 15 si fue enconlado":"El 15 no fue encolado");
-            System.out.println( queue.contains(2)?"El 2 si fue enconlado":"El 2 no fue encolado");// operadores ternarios
-
-        } catch (QueueException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
 }
