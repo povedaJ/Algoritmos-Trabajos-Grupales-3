ea89820dc173e1fa9937e87ebb7c098b26517913
povedaJ <66696920+povedaJ@users.noreply.github.com>
2023-05-13 13:50:27 -0600

    Test

---
+++ Lab_8Grupo3/src/test/java/domain/ComplexTest.java
@@ -0,0 +1,88 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+public class ComplexTest {
+    int tmp[]= new  int [20000];
+
+
+    int a[]= new int [20000];
+    int b[]= new int [20000];
+    int c[]= new int [20000];
+    int d[]= new int [20000];
+    int e[]= new int [20000];
+    int f[]= new int [20000];
+
+    @Test
+    void test() {
+
+        util.Utility.fill(a,5000);
+        util.Utility.fill(b,5000);
+        util.Utility.fill(c,5000);
+        util.Utility.fill(d,5000);
+        util.Utility.fill(e,5000);
+        util.Utility.fill(f,5000);
+        System.out.println("Arrray A content\n "+util.Utility.show(a,200));
+        System.out.println("Arrray B content\n "+util.Utility.show(b,200));
+        System.out.println("Arrray C content\n "+util.Utility.show(c,200));
+        System.out.println("Arrray D content\n "+util.Utility.show(d,200));
+        System.out.println("Arrray E content\n "+util.Utility.show(e,200));
+        System.out.println("Arrray F content\n "+util.Utility.show(f,200));
+
+        Complex complex= new Complex();
+        System.out.println("Esta ordenado: "+ isSorted(a));
+        if(!isSorted(a)){
+            complex.quickSort(a,0,199);
+            System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(a,200));
+        }
+        System.out.println("Esta ordenado: "+ isSorted(a));
+        System.out.println("Esta ordenado: "+ isSorted(b));
+        if(!isSorted(b)){
+            complex.shellSort(b);
+            System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(b,200));
+        }
+        System.out.println("Esta ordenado: "+ isSorted(b));
+        System.out.println("Esta ordenado: "+ isSorted(c));
+        if(!isSorted(c)){
+            complex.mergeSort(c,tmp,0,199);
+            System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(c,200));
+            vaciarArreglo(tmp);
+        }
+        System.out.println("Esta ordenado: "+ isSorted(c));
+        System.out.println("Esta ordenado: "+ isSorted(d));
+        if(!isSorted(d)){
+            complex.shellSort(d);
+            System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(d,200));
+        }
+        System.out.println("Esta ordenado: "+ isSorted(d));
+        System.out.println("Esta ordenado: "+ isSorted(e));
+        if(!isSorted(e)){
+            complex.radixSort(e,20000);
+            System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(e,200));
+        }
+        System.out.println("Esta ordenado: "+ isSorted(e));
+        System.out.println("Esta ordenado: "+ isSorted(f));
+        if(!isSorted(f)){
+            complex.mergeSort(f,tmp,0,199);
+            System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(f,200));
+            vaciarArreglo(tmp);
+        }
+        System.out.println("Esta ordenado: "+ isSorted(f));
+
+
+
+    }
+    private boolean isSorted(int[] a) {
+        int i= a.length;
+        for (int j = 0; j < i-1; j++) {
+            if (util.Utility.compare(a[j],a[j+1])>0){ return false;}
+        }
+        return true;
+
+    }
+    public void vaciarArreglo(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = 0;
+        }
+    }
+}
--- Lab_8Grupo3/src/test/java/domain/ElementaryTest.java
+++ Lab_8Grupo3/src/test/java/domain/ElementaryTest.java
@@ -5,12 +5,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ElementaryTest {
+    int a[]= new int [10000];
+    int b[]= new int [10000];
+    int c[]= new int [10000];
+    int d[]= new int [10000];
+    int e[]= new int [10000];
+    int f[]= new int [10000];
 
     @Test
     void test() {
-        int a[]= new int [10000];
+
         util.Utility.fill(a,5000);
-        System.out.println("Arrray content\n "+util.Utility.show(a,200));
+        util.Utility.fill(b,5000);
+        util.Utility.fill(c,5000);
+        util.Utility.fill(d,5000);
+        util.Utility.fill(e,5000);
+        util.Utility.fill(f,5000);
+        System.out.println("Arrray A content\n "+util.Utility.show(a,200));
+        System.out.println("Arrray B content\n "+util.Utility.show(b,200));
+        System.out.println("Arrray C content\n "+util.Utility.show(c,200));
+        System.out.println("Arrray D content\n "+util.Utility.show(d,200));
+        System.out.println("Arrray E content\n "+util.Utility.show(e,200));
+        System.out.println("Arrray F content\n "+util.Utility.show(f,200));
+
         Elementary elementary = new Elementary();
         System.out.println("Esta ordenado: "+ isSorted(a));
         if(!isSorted(a)){
@@ -18,6 +35,36 @@ class ElementaryTest {
             System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(a,200));
         }
         System.out.println("Esta ordenado: "+ isSorted(a));
+        System.out.println("Esta ordenado: "+ isSorted(b));
+        if(!isSorted(b)){
+            elementary.bubbleSort(b);
+            System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(b,200));
+        }
+        System.out.println("Esta ordenado: "+ isSorted(b));
+        System.out.println("Esta ordenado: "+ isSorted(c));
+        if(!isSorted(c)){
+            elementary.selectionSort(c);
+            System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(c,200));
+        }
+        System.out.println("Esta ordenado: "+ isSorted(c));
+        System.out.println("Esta ordenado: "+ isSorted(d));
+        if(!isSorted(d)){
+            elementary.improvedBubbleSort(d);
+            System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(d,200));
+        }
+        System.out.println("Esta ordenado: "+ isSorted(d));
+        System.out.println("Esta ordenado: "+ isSorted(e));
+        if(!isSorted(e)){
+            elementary.improvedBubbleSort(e);
+            System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(e,200));
+        }
+        System.out.println("Esta ordenado: "+ isSorted(e));
+        System.out.println("Esta ordenado: "+ isSorted(f));
+        if(!isSorted(f)){
+            elementary.improvedBubbleSort(f);
+            System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(f,200));
+        }
+        System.out.println("Esta ordenado: "+ isSorted(f));
 
 
     }
@@ -26,8 +73,25 @@ class ElementaryTest {
         int i= a.length;
         for (int j = 0; j < i-1; j++) {
             if (util.Utility.compare(a[j],a[j+1])>0){ return false;}
-            }
-            return true;
+        }
+        return true;
 
     }
+//    @Test
+//    void testClase() {
+//        int a[]= new int [10000];
+//        util.Utility.fill(a,5000);
+//        System.out.println("Arrray content\n "+util.Utility.show(a,200));
+//        Elementary elementary = new Elementary();
+//        System.out.println("Esta ordenado: "+ isSorted(a));
+//        if(!isSorted(a)){
+//            elementary.improvedBubbleSort(a);
+//            System.out.println("\nArrray content Improve Bubble Sort\n "+util.Utility.show(a,200));
+//        }
+//        System.out.println("Esta ordenado: "+ isSorted(a));
+//
+//
+//    }
+
+
 }
