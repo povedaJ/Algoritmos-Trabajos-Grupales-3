9f6fddcecf285e9411c6c97f31262cc80f7cf41b
povedaJ <66696920+povedaJ@users.noreply.github.com>
2023-05-25 14:49:03 -0600

    test

--- I.S.2023.APA.IF-3001.021-Lab9/src/test/java/domain/BTreeTest.java
+++ I.S.2023.APA.IF-3001.021-Lab9/src/test/java/domain/BTreeTest.java
@@ -40,35 +40,35 @@ class BTreeTest {
 
     }
 
-    @Test
-    void test() {
-        BTree bTree = new BTree();
-        try {
-            //bTree.add(10);
-            //bTree.add(20);
-            for (int i = 0; i < 10; i++) {
-                bTree.add(util.Utility.random(50));
-            }
-            System.out.println(bTree);
-            System.out.println("BTree size: "+bTree.size());
-            for (int i = 0; i <10 ; i++) {
-                int value = util.Utility.random(50);
-                boolean contains = bTree.contains(value);
-                System.out.println(contains
-                        ?"The value ["+value+"] exists. Height: "+bTree.height(value)
-                        :"The value ["+value+"] does not exist");
-                if(contains){
-                    System.out.println("The value ["+value+"] was removed");
-                    bTree.remove(value);
-                }
-            }
-            System.out.println("Tree height: "+bTree.height());
-            System.out.println(bTree);
-
-
-        } catch (TreeException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    @Test
+//    void test() {
+//        BTree bTree = new BTree();
+//        try {
+//            //bTree.add(10);
+//            //bTree.add(20);
+//            for (int i = 0; i < 10; i++) {
+//                bTree.add(util.Utility.random(50));
+//            }
+//            System.out.println(bTree);
+//            System.out.println("BTree size: "+bTree.size());
+//            for (int i = 0; i <10 ; i++) {
+//                int value = util.Utility.random(50);
+//                boolean contains = bTree.contains(value);
+//                System.out.println(contains
+//                        ?"The value ["+value+"] exists. Height: "+bTree.height(value)
+//                        :"The value ["+value+"] does not exist");
+//                if(contains){
+//                    System.out.println("The value ["+value+"] was removed");
+//                    bTree.remove(value);
+//                }
+//            }
+//            System.out.println("Tree height: "+bTree.height());
+//            System.out.println(bTree);
+//
+//
+//        } catch (TreeException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
 }
